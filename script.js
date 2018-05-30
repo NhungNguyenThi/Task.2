7a3696e2c59f0fbe65c62ffc0a22bbf4ec9e084f
NhungNguyenThi <39510062+NhungNguyenThi@users.noreply.github.com>
2018-05-30 17:35:52 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+  
+
   var countTime = 25;
 	var breakTime = 5;
-	var pause = false;
+	var pause = false;//work
 	var seconds = 0;
 	var minutes = 25;
 	var counting;
 
 document.getElementById("timer").innerHTML = minutes+ ":00";
-var x = document.getElementById("title").textContent;
+document.getElementById("count").value = countTime;
+document.getElementById("break").value = breakTime;
 function countDown(){
+  var x = document.getElementById("title").textContent;
   if(minutes == 0 && seconds == 0){
     if(x == "Session"){
       document.getElementById("title").innerHTML="Break";
@@ -31,6 +35,8 @@ function countDown(){
       }
     }
 }
+
+
 document.getElementById("startbtn").addEventListener("click", function(){
   if(pause == false){
      counting = setInterval(countDown, 1000);
@@ -43,3 +49,58 @@ document.getElementById("pausebtn").addEventListener("click", function(){
      pause = false;
    }
 }); 
+
+
+function minusBreak(){
+  if(pause == false){
+    if(breakTime > 1){
+      breakTime--;
+      document.getElementById("break").innerHTML=breakTime
+      document.getElementById("title").innerHTML="Session";
+      document.getElementById("timer").innerHTML = minutes+ ":00";
+      seconds = 0;
+      minutes = countTime;
+    }
+  }
+}
+
+function plusBreak() {
+  if(pause == false){
+    if(breakTime < 10){
+    breakTime++;
+    document.getElementById("break").innerHTML=breakTime;
+    document.getElementById("title").innerHTML="Session";
+    document.getElementById("timer").innerHTML = minutes+ ":00";
+    seconds = 0;
+    minutes = countTime;
+    }
+  }
+}
+
+
+function minusCount() {
+  if(pause == false){
+    if(countTime > 1){
+      countTime--;
+        document.getElementById("count").innerHTML = countTime;
+        document.getElementById("timer").innerHTML = countTime + ":00";
+        document.getElementById("title").innerHTML="Session";
+    }
+    seconds = 0;
+    minutes = countTime;
+  }
+}
+
+function plusCount() {
+  if(pause == false){
+    if(countTime < 60){
+      countTime++;
+        document.getElementById("count").innerHTML = countTime;
+        document.getElementById("timer").innerHTML = countTime + ":00";
+        document.getElementById("title").innerHTML="Session";
+        seconds = 0;
+        minutes = countTime;
+    }
+  }
+}  
+
