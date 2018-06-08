26b115e6bfbb624855d62d4d0892f57a4c3e0ff9
NhungNguyenThi <39510062+NhungNguyenThi@users.noreply.github.com>
2018-06-08 10:26:42 +0700

    Add files via upload

--- form.js
+++ form.js
@@ -1,17 +1,18 @@
+
 function validation(){
 
 	var user = document.getElementById('username').value;
-	var password = document.getElementById('password').value;
-	var confrmpassword = document.getElementById('passwordconfrm').value;
+	var pass = document.getElementById('password').value;
+	var conpassword = document.getElementById('passwordconfrm').value;
 	var number = document.getElementById('numbermobile').value;
 	var email = document.getElementById('emailid').value;
 
-	if( user == "" ){
+	if(user == ""){
 		document.getElementById('usernameid').innerHTML = "*Please fill the username";
 		return false;
 	}
-	if(( user.length < 3) || (user.length > 20)){
-		document.getElementById('usernameid').innerHTML = "*Please fill the username > 3 char";
+	if((user.length < 3) || (user.length > 20)){
+		document.getElementById('usernameid').innerHTML = "*Please fill the username between 3 and 20 char";
 		return false;
 	}
 
@@ -20,42 +21,42 @@ function validation(){
 		return false;
 	}
 
-	if( password == "" ){
+	if(pass == ""){
 		document.getElementById('passwords').innerHTML = "*Please fill the password";
 		return false;
 	}
 
-	if((password.length < 5) || (password.length > 20)){
+	if((pass.length < 5) || (pass.length > 20)){
 		document.getElementById('passwords').innerHTML = "*Please fill the password between 5 and 20";
 		return false;
 	}
 
-	if( password!=confrmpassword){
-		document.getElementById('confrmpassword').innerHTML = "*Password are not matching";
+	if(pass!=conpassword){
+		document.getElementById('confrmpassword').innerHTML = "*Password are not same";
 		return false;
 	}
 
-	if( confrmpassword == "" ){
-		document.getElementById('confrmpassword').innerHTML = "*Please fill the confrm paasword";
+	if(conpassword == ""){
+		document.getElementById('confrmpassword').innerHTML = "*Please fill the confirm paasword";
 		return false;
 	}
 
-	if( number == "" ){
-		document.getElementById('mobilenumber').innerHTML = "*Please fill the mobile number ";
+	if(number == ""){
+		document.getElementById('mobilenumber').innerHTML = "*Please fill the number ";
 		return false;
 	}
 
-	if(number.length<10 || number.length>11){
-		document.getElementById('mobilenumber').innerHTML = "*Mobile number should be 10 digits";
+	if(number.length < 10 || number.length > 11){
+		document.getElementById('mobilenumber').innerHTML = "*Number should be 10->11 digits";
 		return false;
 	}
 
 	if(isNaN(number)){
-		document.getElementById('mobilenumber').innerHTML = "*Mobile number should conatins only digits ";
+		document.getElementById('mobilenumber').innerHTML = "*Number should conatins only digits ";
 		return false;
 	}
 
-	if( email == "" ){
+	if(email == ""){
 		document.getElementById('emailids').innerHTML = "*Please fill the email id";
 		return false;
 	}
@@ -67,7 +68,7 @@ function validation(){
 	}
 
 	if((email.charAt(email.length-4)!='.')){
-		document.getElementById('emailids').innerHTML = " ** please fill the email id in proper format . ";
+		document.getElementById('emailids').innerHTML = "*Please fill the email id in proper format";
 		return false;
 	} 
 }
